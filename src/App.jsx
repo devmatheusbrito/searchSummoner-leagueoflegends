d26e9781d7489b047769cb3220c2c8231fe3b76b
matheusbrito07 <matheusrj.dev@gmail.com>
2022-08-24 20:22:49 -0300

    att css add mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,58 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Alert from '@mui/material/Alert';
 
 function App() {
-const [inputValue, setInputValue] = useState("")
-const [summonerData, setSummonerData] = useState('')
+  const [inputValue, setInputValue] = useState("")
+  const [summonerData, setSummonerData] = useState('')
+  const [alertError, setAlertError] = useState(false)
 
-const key = 'RGAPI-bba63c52-cc78-4f65-9ec0-0bf86ba9f789'
-const api = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
-
-async function getSummoner() {
-    const searchSummoner = `${api}${inputValue}?api_key=${key}`
+  async function getSummoner() {
+    try{
+      const searchSummoner = `${api}${inputValue}?api_key=${key}`
     await axios
       .get(searchSummoner)
       .then((response) => setSummonerData(response.data))
-      .catch((err) => {console.error("please no", err)});
-  } 
-useEffect(() => {
-  getSummoner()
-}, []);
-console.log('XDDDD', summonerData)
+    } catch(error) {
+        setAlertError(true)
+    }
+  }
 
-return (
-  <div className="App">
-    <div className="container">
-      <h6>SEARCH SUMMONER LEAGUE OF LEGENDS</h6>
-      <input type="text" onChange={e => setInputValue(e.target.value)}/>
-      <button onClick={e => getSummoner(e)}> search </button>
-      {summonerData.length > 0
-      ? 
-        <p>DONT HAVE PLAYER</p>
-      : 
-        <p>GREAT HAVE PLAYER</p>
-      }
+  useEffect(() => {
+    getSummoner()
+  }, []);
+
+  console.log('aaaaaaaaa', alertError)
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>SEARCH SUMMONER LEAGUE OF LEGENDS</h2>
+        <div className="inputSearchContent">
+          <TextField  size="small" label="User" variant="outlined"type="text" onChange={e => setInputValue(e.target.value)} />
+          <Button className="btn-search" size="small" variant="outlined" onClick={e => getSummoner(e)}> search </Button>
+        </div>
         {summonerData.name
-        ? 
-          <div className="container-content">
-            <h2>Nick: {summonerData.name}</h2>
-            <p>Level Account: {summonerData.summonerLevel}</p>
-            <img src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/profileicon/${summonerData.profileIconId}.png`}/>
-          </div>
-        :
-          <p></p>
+          ?
+            <div className="cardContent">
+              <div className="contentAvatar">
+                <img className='avatarIcon' src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/profileicon/${summonerData.profileIconId}.png`} />
+              </div>
+              <div className="content-text">
+                <div>name: {summonerData.name}</div>
+                <div>level: {summonerData.summonerLevel}</div>
+              </div>
+            </div>
+          :
+            <p></p>
         }
-  
-      
+      </div>
     </div>
-    
-  </div>
-)
+  )
 }
 
 export default App
